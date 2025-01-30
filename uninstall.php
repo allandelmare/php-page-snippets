32b06a730696d6c2d5aab4adda84c47984ee0901
allandelmare <8284237+allandelmare@users.noreply.github.com>
2025-01-29 19:59:45 -0500

    simplify plugin

--- uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// If uninstall not called from WordPress, exit
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
-}
-
-// Clean up by deleting all snippet meta data
-global $wpdb;
-$wpdb->delete($wpdb->postmeta, array('meta_key' => '_php_snippet')); 
