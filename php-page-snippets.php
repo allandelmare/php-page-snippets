855e73c800a58f945e3980a7cfbe0bec09805f47
allandelmare <8284237+allandelmare@users.noreply.github.com>
2025-01-30 12:27:39 -0500

    Update php-page-snippets.php

--- php-page-snippets.php
+++ php-page-snippets.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: PHP Page Snippets
- * Description: Adds a metabox to pages for PHP code snippets that run only on that page and allows adding code to functions.php
- * Version: 1.1.0
+ * Description: Adds metaboxes to pages for PHP and JavaScript code snippets that run only on that page and allows adding code to functions.php
+ * Version: 1.2.0
  * Author: Allan Delmare
  * License: GPL v2 or later
  */
@@ -22,13 +22,18 @@ class PHP_Page_Snippets {
         add_action('save_post', array($this, 'save_php_snippet'));
         add_action('the_content', array($this, 'execute_php_snippet'));
         
-        // New functions.php snippet functionality
+        // JavaScript functionality
+        add_action('add_meta_boxes', array($this, 'add_js_snippet_meta_box'));
+        add_action('save_post', array($this, 'save_js_snippet'));
+        add_action('wp_head', array($this, 'output_js_snippet'), 99); // Priority 99 to load late in head
+        
+        // Functions.php snippet functionality
         add_action('admin_menu', array($this, 'add_settings_page'));
         add_action('admin_init', array($this, 'register_settings'));
         add_action('init', array($this, 'execute_functions_snippet'));
     }
 
-    // Original meta box methods
+    // PHP Meta box methods
     public function add_php_snippet_meta_box() {
         add_meta_box(
             'php_snippet_meta_box',
@@ -89,7 +94,60 @@ class PHP_Page_Snippets {
         return $content;
     }
 
-    // New settings page methods
+    // JavaScript Meta box methods
+    public function add_js_snippet_meta_box() {
+        add_meta_box(
+            'js_snippet_meta_box',
+            'JavaScript Snippet',
+            array($this, 'display_js_snippet_meta_box'),
+            'page',
+            'normal',
+            'high'
+        );
+    }
+
+    public function display_js_snippet_meta_box($post) {
+        wp_nonce_field('js_snippet_nonce', 'js_snippet_nonce');
+        $snippet = get_post_meta($post->ID, '_js_snippet', true);
+        ?>
+        <p><em>Enter JavaScript code without &lt;script&gt; tags. This code will be added to this page's &lt;head&gt; section.</em></p>
+        <textarea name="js_snippet" id="js_snippet" style="width: 100%; height: 200px; font-family: monospace;"><?php echo esc_textarea($snippet); ?></textarea>
+        <?php
+    }
+
+    public function save_js_snippet($post_id) {
+        if (!isset($_POST['js_snippet_nonce']) || 
+            !wp_verify_nonce($_POST['js_snippet_nonce'], 'js_snippet_nonce')) {
+            return;
+        }
+
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
+        if (!current_user_can('edit_page', $post_id)) {
+            return;
+        }
+
+        if (isset($_POST['js_snippet'])) {
+            $snippet = wp_unslash($_POST['js_snippet']);
+            update_post_meta($post_id, '_js_snippet', $snippet);
+        }
+    }
+
+    public function output_js_snippet() {
+        if (is_page()) {
+            $snippet = get_post_meta(get_the_ID(), '_js_snippet', true);
+            if (!empty($snippet)) {
+                echo "\n<!-- Page-specific JavaScript -->\n";
+                echo '<script>';
+                echo "\n" . $snippet . "\n";
+                echo '</script>' . "\n";
+            }
+        }
+    }
+
+    // Settings page methods
     public function add_settings_page() {
         add_options_page(
             'PHP Snippets Settings',
