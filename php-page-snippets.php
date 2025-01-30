eb858ad841e65bf5cd32b8c7836a6f0a3ff269c8
allandelmare <8284237+allandelmare@users.noreply.github.com>
2025-01-29 19:50:22 -0500

    Initial commit

---
+++ php-page-snippets.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Plugin Name: PHP Page Snippets
+ * Plugin URI: https://github.com/allandelmare/php-page-snippets
+ * Description: Adds a metabox to pages for PHP code snippets that run only on that page
+ * Version: 1.0.0
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Author: Allan Delmare
+ * Author URI: https://github.com/allandelmare
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: php-page-snippets
+ * Domain Path: /languages
+ *
+ * PHP Page Snippets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * PHP Page Snippets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PHP Page Snippets. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class PHP_Page_Snippets {
+    
+    public function __construct() {
+        add_action('add_meta_boxes', array($this, 'add_php_snippet_meta_box'));
+        add_action('save_post', array($this, 'save_php_snippet'));
+        add_action('the_content', array($this, 'execute_php_snippet'));
+    }
+
+    public function add_php_snippet_meta_box() {
+        add_meta_box(
+            'php_snippet_meta_box',
+            'PHP Snippet',
+            array($this, 'display_php_snippet_meta_box'),
+            'page',
+            'normal',
+            'high'
+        );
+    }
+
+    public function display_php_snippet_meta_box($post) {
+        // Add nonce for security
+        wp_nonce_field('php_snippet_nonce', 'php_snippet_nonce');
+        
+        // Get existing snippet if any
+        $snippet = get_post_meta($post->ID, '_php_snippet', true);
+        ?>
+        <p><em>Enter PHP code without <?php echo esc_html('<?php'); ?> tags. This code will only run on this page's frontend.</em></p>
+        <textarea name="php_snippet" id="php_snippet" style="width: 100%; height: 200px; font-family: monospace;"><?php echo esc_textarea($snippet); ?></textarea>
+        <?php
+    }
+
+    public function save_php_snippet($post_id) {
+        // Check if nonce is set and valid
+        if (!isset($_POST['php_snippet_nonce']) || 
+            !wp_verify_nonce($_POST['php_snippet_nonce'], 'php_snippet_nonce')) {
+            return;
+        }
+
+        // Check if this is an autosave
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
+        // Check user permissions
+        if (!current_user_can('edit_page', $post_id)) {
+            return;
+        }
+
+        // Sanitize and save the snippet
+        if (isset($_POST['php_snippet'])) {
+            $snippet = wp_unslash($_POST['php_snippet']);
+            update_post_meta($post_id, '_php_snippet', $snippet);
+        }
+    }
+
+    public function execute_php_snippet($content) {
+        // Only execute on single pages in the main query
+        if (is_page() && is_main_query() && in_the_loop()) {
+            $snippet = get_post_meta(get_the_ID(), '_php_snippet', true);
+            
+            if (!empty($snippet)) {
+                ob_start();
+                try {
+                    eval($snippet);
+                } catch (ParseError $e) {
+                    if (current_user_can('edit_pages')) {
+                        echo '<!-- PHP Snippet Error: ' . esc_html($e->getMessage()) . ' -->';
+                    }
+                }
+                $snippet_output = ob_get_clean();
+                $content .= $snippet_output;
+            }
+        }
+        return $content;
+    }
+}
+
+// Initialize the plugin
+new PHP_Page_Snippets(); 
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== PHP Page Snippets ===
+Contributors: YOUR_WP_USERNAME
+Tags: php, snippets, code, page
+Requires at least: 5.0
+Tested up to: 6.4
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Add PHP code snippets to individual WordPress pages through a simple metabox interface.
+
+== Description ==
+
+PHP Page Snippets adds a metabox to WordPress pages where administrators can add PHP code snippets that will only execute on that specific page. This is useful for adding custom PHP functionality to individual pages without modifying theme files.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/php-page-snippets` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Edit any page to find the PHP Snippet metabox where you can add your code
+
+== Frequently Asked Questions ==
+
+= Is it safe to allow PHP execution? =
+
+Only users with the `edit_pages` capability can add or edit PHP snippets. Make sure you trust your page editors.
+
+== Screenshots ==
+
+1. PHP Snippet metabox in the page editor
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial release 
